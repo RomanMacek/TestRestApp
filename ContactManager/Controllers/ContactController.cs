9d620a8179b1daa4a20a36b2bbc375e71f10d267
Roman Macek <>
2018-07-19 09:36:23 +0200

    Vytvoreni Read only projektu

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Models;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
