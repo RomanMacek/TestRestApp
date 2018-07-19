b2250a81f52c26ab274a61a63e0faaa31684878a
Roman Macek <roman.macek@valatron.com>
2018-07-19 13:40:46 +0200

    MetodaPost: vytvoreni metody

--- ContactManager/Controllers/ContactController.cs
+++ ContactManager/Controllers/ContactController.cs
@@ -22,5 +22,13 @@ namespace ContactManager.Controllers
         {
             return contactRepository.GetAllContacts();
         }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            return response;
+        }
+
     }
 }
